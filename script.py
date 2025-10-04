37eb5536ba7cfb75de376a04e792c89d165cde87
Bayi-Mostapha <bayimostapha@gmail.com>
2025-10-04 19:36:10 +0100

    stored credentials in env var, and made code work with functions

--- script.py
+++ script.py
@@ -1,47 +1,59 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
+import os
 
 data = pd.read_csv("./data/student_exam_scores.csv")
 
-data.previous_scores = data.previous_scores.astype(float)
-data.student_id = pd.Series(range(1, len(data) + 1), dtype=int)
+def transform():
+    data.previous_scores = data.previous_scores.astype(float)
+    data.student_id = pd.Series(range(1, len(data) + 1), dtype=int)
 
-def check_type(df):
-    assert pd.api.types.is_integer_dtype(df.student_id)
-    assert pd.api.types.is_float_dtype(df.hours_studied)
-    assert pd.api.types.is_float_dtype(df.sleep_hours)
-    assert pd.api.types.is_float_dtype(df.attendance_percent)
-    assert pd.api.types.is_float_dtype(df.previous_scores)
-    assert pd.api.types.is_float_dtype(df.exam_score)
+def check_type():
+    assert pd.api.types.is_integer_dtype(data.student_id)
+    assert pd.api.types.is_float_dtype(data.hours_studied)
+    assert pd.api.types.is_float_dtype(data.sleep_hours)
+    assert pd.api.types.is_float_dtype(data.attendance_percent)
+    assert pd.api.types.is_float_dtype(data.previous_scores)
+    assert pd.api.types.is_float_dtype(data.exam_score)
     print("All types are good ✅")
 
-check_type(data)
+def connect_db():
+    username = os.getenv('DB_USR')
+    password = os.getenv('DB_PWD')
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    database = "students"
+    try:
+        return create_engine(f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}")
+    except SQLAlchemyError as e:
+        print("SQLAlchemy error:", e)
+        exit(0)
+    except Exception as e:
+        print("Unexpected error:", e)
+        exit(0)
 
-username = "admin"
-password = "admin"
-host = "db"
-port = 5432
-database = "students"
+def load(engine):
+    if not engine:
+        exit(0)
+    try:
+        data.to_sql(
+            "students",
+            engine,
+            if_exists="replace",
+            index=False
+        )
+        print("Data loaded successfully ✅")
+    except SQLAlchemyError as e:
+        print("SQLAlchemy error:", e)
+    except Exception as e:
+        print("Unexpected error:", e)
 
-try:
-    # Create the engine
-    engine = create_engine(f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}")
-    
-    # Push DataFrame to PostgreSQL
-    data.to_sql(
-        "students",
-        engine,
-        if_exists="replace",
-        index=False
-    )
-    
-    print("Data loaded successfully ✅")
+def main():
+    transform()
+    check_type()
+    e = connect_db()
+    load(e)
 
-except SQLAlchemyError as e:
-    # Catch SQLAlchemy-specific errors
-    print("SQLAlchemy error:", e)
-
-except Exception as e:
-    # Catch other general errors
-    print("Unexpected error:", e)
+if __name__ == "__main__":
+    main()
